b678268d47e0476460f02ac4d0701576e9774e38
JunpengCode <jpzhu.gm@gmail.com>
2018-08-24 22:18:03 +0800

    Add

---
+++ Technical Documents/OS/linux_system_programming/code/testRead.c
@@ -0,0 +1,9 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main (int argc,char *argv[]){
+  int fd;   // It is the File Description which is used int the Linux System.
+  return 0;
+}
