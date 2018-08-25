81db19100afe1d6d8b5b5c14ef28500d133e7b0f
JunpengCode <jpzhu.gm@gmail.com>
2018-08-25 16:10:35 +0800

    modify the reading example for testRead.c

--- Technical Documents/OS/linux_system_programming/code/file/testRead.c
+++ Technical Documents/OS/linux_system_programming/code/file/testRead.c
@@ -4,11 +4,12 @@
 #include <stdio.h>  //It includes perror(const char *str)
 #include <unistd.h>   //It includes read(int fd, void* buf, size_t len)
 #include <errno.h>
+#include <string.h>  //It includes memset(void *str, int value, size_t __n)
 #define BUFFER_SIZE 4096
 
 int main (int argc,char *argv[]){
   int fd;   // It is the File Description which is used int the Linux System.
-  fd = open("/home/os/Papers/Technical Documents/OS/linux_system_programming/code/testRead.c",O_RDONLY);
+  fd = open("/home/os/Papers/Technical Documents/OS/linux_system_programming/code/file/testRead.c",O_RDONLY);
   if (fd == -1){
     perror("main");
   }else{
@@ -17,17 +18,16 @@ int main (int argc,char *argv[]){
 
   size_t ret;
 
-  char buf[BUFFER_SIZE];
-  size_t len = 100;
-  while (len != 0 && (ret = (read(fd,&buf,BUFFER_SIZE))) != 0){
+  char buf[BUFFER_SIZE];   //It means the buffer for the reading operating.
+  memset(&buf,0,BUFFER_SIZE);  //It means the initial value for every value in the buffer
+
+  while ((ret = (read(fd,&buf,BUFFER_SIZE))) != 0){
     if (ret == -1){
        if (errno == EINTR)   //It means interupts
            continue;
        perror("main");
        break;
     }
-//    len -= ret; 
-//    buf += ret; 
     for (int i = 0 ; i < BUFFER_SIZE; i++){
       printf("%c",buf[i]);
     }
