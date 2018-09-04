7121736b6a4297641a683b10a9ffcf3b039498bb
JunpengCode <jpzhu.gm@gmail.com>
2018-09-04 17:39:33 +0800

    Add: adding the mmap() function

---
+++ Technical-Documents/OS/Linux_System_Programming/code/file/testMmap.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#define BUFFER_SIZE 4096
+int main(int argc, char *argv[]){
+  ssize_t fd;
+  fd = open("/home/os/Papers/Technical-Documents/OS/Linux_System_Programming/code/file/testInt",O_RDWR);
+  char* p = (char *)mmap(NULL,BUFFER_SIZE,PROT_READ | PROT_WRITE,MAP_SHARED,fd,0);
+  for (int i = 0; i < BUFFER_SIZE; i++){
+    printf("%c",(char) p[i]);
+  }
+  printf("\n");
+  return 0;
+}
+
