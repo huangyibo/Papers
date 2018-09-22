acbc747bb2914a50345dfca16ff053ed743c95cf
JunpengCode <jpzhu.gm@gmail.com>
2018-09-22 22:04:09 +0800

    modify

--- Technical-Documents/CUDA Programming/codes/_cudaDriverVersionDriverAPI.c
+++ Technical-Documents/CUDA Programming/codes/_cudaDriverVersionDriverAPI.c
@@ -6,6 +6,6 @@ int main(){
   int driverVersion;
   cuInit(0);
   cuDriverGetVersion(&driverVersion);
-  printf("CUDA Driver Version:%d.%d\n",driverVersion/1000,driverVersion/100);
+  printf("CUDA Driver Version:%d.%d%d\n",driverVersion/1000,driverVersion/100,driverVersion%10);
   return 0;
 }
