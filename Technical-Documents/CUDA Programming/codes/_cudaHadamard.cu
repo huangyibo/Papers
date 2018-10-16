95c06a460275e3b846f9229dc90931c2a30d84d9
JunpengCode <jpzhu.gm@gmail.com>
2018-10-16 22:46:39 +0800

    modify

--- Technical-Documents/CUDA Programming/codes/_cudaHadamard.cu
+++ Technical-Documents/CUDA Programming/codes/_cudaHadamard.cu
@@ -1,3 +1,4 @@
+//nvcc -std=c++11 -o test -I/usr/local/cuda/samples/common/inc -I.  _cudaHadamard.cu -lcuda
 #include <newton/newton.hpp>
 
 int main()
@@ -9,8 +10,10 @@ int main()
   newton::numeric_vector<float> A = a;
   newton::numeric_vector<float> B = b;
   newton::numeric_vector<float> C = c;
-
-  C = A * B;
+  for (int i = 0 ; i < 100000000; i++){
+     C = A * B;
+  }
+  //C = A * B;
   thrust::copy(C.begin(), C.end(), std::ostream_iterator<float>(std::cout, " "));
   std::cout << std::endl;
   return 0;
