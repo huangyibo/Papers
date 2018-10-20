4387c6eab12e84d3a1bcbb7d424a26f16ccb21fb
JunpengCode <jpzhu.gm@gmail.com>
2018-10-20 21:06:27 +0800

    Add: adding the dynamic linking

---
+++ Technical-Documents/SharedObjects/Lib.c
@@ -0,0 +1,7 @@
+// gcc -fPIC -shared -o Lib.so Lib.c
+//上述指令生成Lib.so文件之后，Lib.c文件就可以移除，当Lib.c实现改变之后，值需要重新生成Lib.so即可。这样做的前提是接口没有变化，只是接口的实现发生了变化。
+#include <stdio.h>
+
+void foobar(int i){
+  printf("Printing from Lib.so %d\n",i);
+}
