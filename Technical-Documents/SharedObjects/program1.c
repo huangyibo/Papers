59a291efa11ae2c5e8d796e33ae63d23aceb2353
JunpengCode <jpzhu.gm@gmail.com>
2018-10-20 22:45:02 +0800

    Add: add the shared library -l

--- Technical-Documents/SharedObjects/program1.c
+++ Technical-Documents/SharedObjects/program1.c
@@ -1,4 +1,5 @@
 //gcc -o p1 -fPIE program1.c Lib.so  产生地址无关可执行文件，这是重要的地址无关代码技术的一种重要应用
+//gcc -o test program1.c -L. -lfoo   编译时，-lfoo会去-L参数指令的路径下寻找libxxx.so的库，这两个参数组合很重要
 #include "Lib.h"
 
 int main(){
