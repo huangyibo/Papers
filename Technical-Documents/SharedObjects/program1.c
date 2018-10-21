c6b3cb1507f18754b2db80c21f108ecf4b44b05f
JunpengCode <jpzhu.gm@gmail.com>
2018-10-21 17:12:12 +0800

    modify

--- Technical-Documents/SharedObjects/program1.c
+++ Technical-Documents/SharedObjects/program1.c
@@ -1,5 +1,5 @@
 //gcc -o p1 -fPIE program1.c Lib.so  产生地址无关可执行文件，这是重要的地址无关代码技术的一种重要应用
-//gcc -o test program1.c -L. -lfoo   编译时，-lfoo会去-L参数指令的路径下寻找libxxx.so的库，这两个参数组合很重要
+//gcc -o test program1.c -L. -lfoo   编译时，-lfoo会去-L参数指令的路径下寻找libxxx.so的库，这两个参数组合很重要，对于静态链接和动态链接，编译源代码的方式一样，都是采用这条命令
 #include "Lib.h"
 
 int main(){
