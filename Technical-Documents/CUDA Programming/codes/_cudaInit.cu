8e22802c343fc943294c35bee111febe69779859
JunpengCode <jpzhu.gm@gmail.com>
2018-10-26 11:43:51 +0800

    modify

--- Technical-Documents/CUDA Programming/codes/_cudaInit.cu
+++ Technical-Documents/CUDA Programming/codes/_cudaInit.cu
@@ -1,26 +1,30 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 #include <stdbool.h> 
+#include <string.h>
+
+//打印GPU信息
 void printDeviceProp(const cudaDeviceProp &prop)
 {
     printf("Device Name : %s.\n", prop.name);
-    printf("totalGlobalMem : %d.\n", prop.totalGlobalMem);
-    printf("sharedMemPerBlock : %d.\n", prop.sharedMemPerBlock);
+    printf("totalGlobalMem : %ld.\n", prop.totalGlobalMem);
+    printf("sharedMemPerBlock : %ld.\n", prop.sharedMemPerBlock);
     printf("regsPerBlock : %d.\n", prop.regsPerBlock);
     printf("warpSize : %d.\n", prop.warpSize);
-    printf("memPitch : %d.\n", prop.memPitch);
+    printf("memPitch : %ld.\n", prop.memPitch);
     printf("maxThreadsPerBlock : %d.\n", prop.maxThreadsPerBlock);
     printf("maxThreadsDim[0 - 2] : %d %d %d.\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
     printf("maxGridSize[0 - 2] : %d %d %d.\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
-    printf("totalConstMem : %d.\n", prop.totalConstMem);
+    printf("totalConstMem : %ld.\n", prop.totalConstMem);
     printf("major.minor : %d.%d.\n", prop.major, prop.minor);
     printf("clockRate : %d.\n", prop.clockRate);
-    printf("textureAlignment : %d.\n", prop.textureAlignment);
+    printf("textureAlignment : %ld.\n", prop.textureAlignment);
     printf("deviceOverlap : %d.\n", prop.deviceOverlap);
     printf("multiProcessorCount : %d.\n", prop.multiProcessorCount);
 }
- 
-bool InitCUDA()
+
+
+bool initCUDA()
 {
     //used to count the device numbers
     int count;
@@ -30,17 +34,18 @@ bool InitCUDA()
     printf("cuda device count: %d\n",count);
     if (count == 0) {
         fprintf(stderr, "There is no device.\n");
-        return 1;
+        return false;
     }
  
-    // find the device >= 1.X
+    // 寻找指定的Tesla GPU
     int i;
     for (i = 0; i < count; ++i) {
         cudaDeviceProp prop;
         if (cudaGetDeviceProperties(&prop, i) == cudaSuccess) {
-            if (prop.totalGlobalMem >= 1000) {
-                printDeviceProp(prop);
-                cudaSetDevice(i);
+            printf("Device Name : %s.\n", prop.name);
+            if ( (strcasecmp(prop.name, "Tesla P40")) == 0) {
+               printf("Device Name : %s.\n", prop.name);  //被选定的设备在打印时，应该被连续打印两次
+               break;
             }
         }
     }
@@ -48,15 +53,15 @@ bool InitCUDA()
     // if can't find the device
     if (i == count) {
         fprintf(stderr, "There is no device supporting CUDA 1.x.\n");
-        return 1;
+        return false;
     }
- 
-    return 0;
+    cudaSetDevice(i);
+    return true;
 }
  
 int main(int argc, char const *argv[])
 {
-    if (InitCUDA()) {
+    if (initCUDA()) {
         printf("CUDA initialized.\n");
     }
  
