9d71120e13e66e55e4e57c1de660c8f981568f26
JunpengCode <jpzhu.gm@gmail.com>
2018-10-29 11:18:28 +0800

    modify: criticalSection.2能够正确的执行，并在注释中写出了详细的步骤，criticalSection1.c由于存在不同线程同时写同一个共享内存，因为需要设计读写锁解决线程的同步问题

--- Technical-Documents/Thread-Parallel-Programming/criticalSection/criticalSection1-1.c
+++ Technical-Documents/Thread-Parallel-Programming/criticalSection/criticalSection1-1.c
@@ -2,13 +2,8 @@
   * @author Junpen Zhu
   * 代码实现功能：假设A现在有1500元(sum1)，B现在有500元(sum2)，紧接着A将调用add代码，给B转钱，每次转走200元，相应的自己就减少200元，钱可以为负值
   * 上述过程持续N次，等待结束完成之后，B会调用sub函数，再给自己加300元
-  * 这段代码是错误的，会得到如下的可能结果：
-  * sum1 = -6500
-  * sum2 = -6200
-  **************************************************
-  * sum1 = -6300
-  * sum2 = -6000
-  why? 该问题涉及到一个线程在写共享内存时，另外一个只能读共享内存，所以需要用读写锁来实现互斥访问。
+  * 该问题在criticalSection1.c中提出，主要涉及的问题是线程add和线程sub可能会同时写同一块共享内存（即全局变量），因此一个线程在写时，另外一个线程只能读，两者不能同时执行写操作
+  * 本程序采用读写锁解决线程之间的同步问题。
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,49 +12,44 @@
 
 int sum1 = 1500;
 int sum2 = 500;
+pthread_mutex_t mutex;   //1. 申请全局互斥量，互斥量（或互斥锁）一定要是全局变量
 
 void* add(void* tid){
-  int id = (int)tid;
-  pthread_mutex_t mutex;    //1. 申请mutex变量
-  pthread_mutex_init(&mutex, NULL); //2. 初始化mutex变量，第二个参数恒定为NULL
+  //int id = (int)tid;
   //printf("进入到线程%d.\n", id);
   pthread_mutex_lock(&mutex); //3. 在访问临界区时加锁
   sum2 = sum1 - 200;    //临界区数据发生变化
   sum1 = sum1 - 200;
   pthread_mutex_unlock(&mutex);   //4. 处理完临界区以后，释放锁
-  pthread_mutex_destroy(&mutex);  //5. 释放掉mutex变量，必须要完成以上五步，否则不会得到正确的计算结果  
   return NULL;
 }
 
 void* sub(void* tid){
-  int id = (int)tid;
-  pthread_mutex_t mutex;    //1. 申请mutex变量
-  pthread_mutex_init(&mutex, NULL); //2. 初始化mutex变量，第二个参数恒定为NULL
+  //int id = (int)tid;
   //printf("进入到线程%d.\n", id);
   pthread_mutex_lock(&mutex); //3. 在访问临界区时加锁
   sum2 += 300;
   pthread_mutex_unlock(&mutex);   //4. 处理完临界区以后，释放锁
-  pthread_mutex_destroy(&mutex);  //5. 释放掉mutex变量，必须要完成以上五步，否则不会得到正确的计算结果  
   return NULL;
 
 }
 
 int main(){
+  pthread_mutex_init(&mutex, NULL); //2. 在主线程中初始化mutex变量，第二个参数恒定为NULL
   pthread_t* sumPthread;
   sumPthread = malloc(N*sizeof(pthread_t));
   pthread_t subPthread;
   for (int i = 0; i < N; i++){
     pthread_create(&sumPthread[i], NULL, add, (void*)i);
   }
-  //pthread_create(&subPthread, NULL, sub, (void*)1);
-  for (int i = 0; i < N; i++){
-    pthread_join(sumPthread[i],NULL);
+  pthread_create(&subPthread, NULL, sub, (void*)1);
+  for (int j = 0; j < N; j++){
+    pthread_join(sumPthread[j],NULL);
   } 
-  sub((void*)2);  //将sub改成函数调用还是不对
-  //pthread_join(subPthread,NULL);
+  pthread_join(subPthread,NULL);
 
   printf("sum1 = %d\n", sum1);
   printf("sum2 = %d\n", sum2);
-
+  pthread_mutex_destroy(&mutex);  //5. 释放掉mutex变量，必须要完成以上五步，否则不会得到正确的计算结果
   return 0;
 }
--- Technical-Documents/Thread-Parallel-Programming/criticalSection/criticalSection2.c
+++ Technical-Documents/Thread-Parallel-Programming/criticalSection/criticalSection2.c
@@ -1,10 +1,11 @@
 /**
-  * 该程序也是错误的，会出现下述结果：
-  * sum = 40
-  * sub = 0
-  ***********************************
-  * sum = 39
-  * sub = 1
+  * 首先该段互斥程序能够正确的执行，使用命令gcc -o test criticalSection2.c -lpthread将该段代码编译，生成可执行文件test，接着运行./loop脚本即可在output中看到结果。
+  * 正确的步骤如下：
+  * 1. 申请全局的互斥量pthread_t mutex
+  * 2. 在主线程（main函数）中初始化该互斥量 pthread_mutex_init(&mutex, NULL)
+  * 3. 在互斥量使用前使用 pthread_mutex_lock(&mutex)加锁
+  * 4. 使用完互斥量后释放锁 pthread_mutex_unlock(&mutex);
+  * 5. 在主线程（main函数）中销毁互斥量 pthread_mutex_destroy(&mutex)
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,21 +14,20 @@
 
 int sum = 0;   //全局变量，由不同的应用程序调用可能会产生竞争，被称为临界区，也称为互斥量
 int sub = 40;
+pthread_mutex_t mutex;    //1. 申请mutex变量
 
 void* add(void* tid){
   int id = (int)tid;
-  pthread_mutex_t mutex;    //1. 申请mutex变量
-  pthread_mutex_init(&mutex, NULL); //2. 初始化mutex变量，第二个参数恒定为NULL
   //printf("进入到线程%d.\n", id);
   pthread_mutex_lock(&mutex); //3. 在访问临界区时加锁
   sum += 1;    //临界区数据发生变化
   sub -= 1;
   pthread_mutex_unlock(&mutex);   //4. 处理完临界区以后，释放锁
-  pthread_mutex_destroy(&mutex);  //5. 释放掉mutex变量，必须要完成以上五步，否则不会得到正确的计算结果  
   return NULL;
 }
 
 int main(){
+  pthread_mutex_init(&mutex, NULL); //2. 初始化mutex变量，第二个参数恒定为NULL
   pthread_t *pthread_handles;
   pthread_handles = malloc(N*sizeof(pthread_t));
   for (int i = 0 ; i < N; i++){
@@ -41,5 +41,6 @@ int main(){
   free(pthread_handles);
   printf("sum = %d\n", sum);
   printf("sub = %d\n", sub);
+  pthread_mutex_destroy(&mutex);  //5. 释放掉mutex变量，必须要完成以上五步，否则不会得到正确的计算结果
   return 0;
 }
